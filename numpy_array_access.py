70c6993f645a49d9007b0f6f9ea6d25e9056aa79
anushi2511 <anushi05.ag@gmail.com>
2025-06-18 14:50:32 +0530

    numpy

---
+++ numpy_array_access.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([[[1, 2], [3, 4]], [[5, 6], [7, 8]]])
+print(arr[1, 1, 0])
+
+arr = np.array([1, 2, 3, 4, 5, 6, 7])
+print(arr[:2])
+
+arr = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]])
+print(arr[0:, 1:4])
--- numpy_create_array.py
+++ numpy_create_array.py
@@ -1,22 +1,30 @@
 import numpy as np
 
-#1-D array
-arr = np.array([1, 2, 3, 4, 5])
+arr = np.array([1, 2, 3, 4, 5,6])
 print(arr)
 print(type(arr))
-print(f"Dimension: {arr.ndim}")
+print("Dimension:", arr.ndim)
+arr = arr.reshape(2, 3)
+print(arr)
+print("Shape: ", arr.shape)
 
 #0-D array
 a = np.array(45)
 print(a)
-print(f"Dimension: {a.ndim}")
+print("Dimension:", a.ndim)
 
 #2-D array
 a2 = np.array([[1, 2], [3, 4], [5, 6]])
 print(a2)
-print(f"Dimension: {a2.ndim}")
+print("Dimension:", a2.ndim)
+print("Shape:", a2.shape)
 
 #n-D array
 a3 = np.array([1, 2, 3, 4], ndmin = 5)
 print(a3)
-print(f"Dimension: {a3.ndim}")
+print("Dimension:", a3.ndim)
+print("Shape:", a3.shape)
+
+arr = np.array([[1, 2, 3], [4, 5, 6]])
+newarr = arr.reshape(-1)
+print(newarr)
